e4e5b8781db45f3d45acb70ddbe79cfa63c162a1
Joshua Schmidlkofer <joshland@protonmail.com>
2016-12-27 10:10:28 -0800

    Add Licensing and Copyleft

--- btrfsmaint.py
+++ btrfsmaint.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-# btrfsmaint.py -- periodic maintenance for btrfs filesystems
-# Copyright(c) 2016 Joshua M. Schmidlkofer
-## joshland@protonmail.com
 ##
-## based upon Marc MERLIN's `btrfscrub.sh`
+# btrfsmaint.py -- periodic maintenance for btrfs filesystems
+# Copyright(c) 2016 Joshua M. Schmidlkofer <joshland@protonmail.com>
+# Released under GPLv2.0 (See LICENSE for details)
+##
 
 from __future__ import absolute_import
 from __future__ import division
