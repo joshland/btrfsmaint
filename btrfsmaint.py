4bb5784825a975631788c9985741b8e03e61caf3
Joshua Schmidlkofer <joshuas@itlynk.com>
2017-07-12 16:45:45 -0700

    issue #2: remove unicode_literals issue #1: remove explicit mention of python3 general: complete general efforts to configure logging, and smooth out the developed process for the script

--- btrfsmaint.py
+++ btrfsmaint.py
@@ -1,12 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 # btrfsmaint.py -- periodic maintenance for btrfs filesystems
 # Copyright(c) 2016 Joshua M. Schmidlkofer <joshland@protonmail.com>
 # Released under GPLv2.0 (See LICENSE for details)
-
 from __future__ import print_function
-from __future__ import unicode_literals
-
 
 import click
 import os, sys, subprocess, time, re
@@ -28,6 +25,7 @@ Usage: btrfsmaint.py [-d] [-i] [-t] ( <filesystem> [<filesystem>...]| -a )
   -a              Run for all Filesystems
   <filesystem>    Mountpoint of the BTRFS filesystem.
 """
+LOGROOT="btrfsmaint"
 
 MessagesScrubStopped = [
     re.compile(r"scrub.started.at.+and.was.aborted", re.I|re.M),
@@ -39,71 +37,75 @@ MessagesScrubRunning = [
     re.compile(r"scrub.started.*running.for", re.I|re.M),
     ]
 
-def _cmd_btrfsScrubStatus(volume, debug):
+def gatherRetval(results):
+    if PYTHON3:
+        retval = results.decode('utf-8').strip()
+    else:
+        retval = results.strip()
+        pass
+    return retval
+
+def _cmd_btrfsScrubStatus(volume):
     '''
     Execute 'btrfs scrub status <volume>' 
     
     return output from btrfs scrub status
     '''
-    
+    log = logging.getLogger("%s.scrub" % LOGROOT)
+    log.debug("subprocess start.")
     retval = subprocess.Popen(['btrfs', 'scrub', 'status', volume], stdout=subprocess.PIPE).communicate()[0]
-    if PYTHON3: retval = retval.decode('utf-8')
-    retval = retval.strip()
-    _log.debug("btrfs scrub status:\n%s" % retval)
+    log.debug("subprocess complete.")
+    retval = gatherRetval(retval)
+    log.debug("btrfs scrub status:\n%s" % retval)
     return retval
 
-def _cmd_btrfsBalanceRetval(volume, debug):
+def _cmd_btrfsBalanceRetval(volume):
     '''
     Execute 'btrfs balance status <volume>' 
     
     return output from btrfs balance status
     '''
+    log = logging.getLogger("%s.balance" % LOGROOT)
+    log.debug("subprocess open")
     handle = subprocess.Popen(['btrfs', 'balance', 'status', volume], stdout=subprocess.PIPE)
+    log.debug("subprocess starts")
     handle.wait()
+    log.debug("subprocess complete")
     retval = handle.returncode
-    _log.debug("btrfs balance status returned: %d" % retval)
+    log.info("btrfs balance status returned: %d" % retval)
     return retval
 
-def _cmd_execute(cmdline, debug):
+def _cmd_execute(cmdline):
     '''
     run a command!
     '''
+    log = logging.getLogger("%s.execute" % LOGROOT)
     stderr = ""
     stdout =""
     cmd = cmdline.split(" ")
-    _log.info("cmd execute: %s" % str(cmd))
+    log.debug("cmd execute: %s" % str(cmd))
     handle = subprocess.Popen(cmd, stdout=subprocess.PIPE)
     while 1:
         time.sleep(0.1)
         handle.poll()
         response = handle.communicate()
-        if PYTHON3:
-            out = response[0].decode('utf-8')
-            try:
-                err = response[1].decode('utf-8')
-            except AttributeError:
-                err = ""
-                pass
-        else:
-            out = response[0]
-            try:
-                err = response[1]
-            except AttributeError:
-                err = ""
-                pass
+        out = gatherRetval(response[0])
+        try:
+            err = gatherRetval(response[1])
+        except AttributeError:
+            err = ""
             pass
-        
         stdout   += out
         stderr   += err
         _log.info(out)
-        if len(err): _log.debug('debug: %s' % err)
+        if len(err): log.debug('debug: %s' % err)
         if handle.returncode != None:
             break
         continue
 
     retval = "%s\n%s" % (stdout, stderr)
     retval = retval.strip()
-    _log.debug("cmd returns (%d): \n%s" % (handle.returncode, retval))
+    log.debug("cmd returns (%d): \n%s" % (handle.returncode, retval))
     return retval
 
 def ScrubIsRunning(output):
@@ -139,90 +141,99 @@ def ScrubIsRunning(output):
     return retval
 
 
-def InteractiveRun(cmd, debug):
+def InteractiveRun(cmd):
     '''
     run 'cmd' interactively, giving feedback as it happens
     '''
-    _cmd_execute(cmd, debug = debug)
+    log = logging.getLogger("%s.Interactive" % LOGROOT)
+    _cmd_execute(cmd)
     return True
 
-def LoggerRun(cmd, debug):
+def LoggerRun(cmd):
     '''
     run 'cmd', log important messages
     '''
-    _cmd_execute(cmd, debug = debug)
+    _cmd_execute(cmd)
     return True
 
-def TestRun(cmd, debug):
+def TestRun(cmd):
     '''
     run 'cmd', log important messages
     '''
-    click.echo("WOULD: %s" % cmd)
+    log = logging.getLogger("%s.Test" % LOGROOT)
+    message = "WOULD: %s" % cmd
+    log.info(message)
     return True
 
 def locateBtrfs():
     '''
     local all BTRFS filesystems
     '''
+    log = logging.getLogger("%s.locatebtrfs" % LOGROOT)
     retval = []
     filesystems = {}
     
     contents = open("/proc/mounts", "r").read()
     for x in contents.split("\n"):
         if x.find(" btrfs ") < 0: continue
+        log.debug("located: %s" % x)
         f = x.split(" ")
         if len(f) < 3: continue
         if f[2] != 'btrfs': continue
         if f[1] in retval: continue
         try:
             filesystems[f[0]].append(f[1])
+            log.debug("set examination: %s" % f[1])
         except KeyError:
             filesystems[f[0]] = [ f[1], ]
+            log.debug("Error Resolving Key: %s: %s" % (f[0],f[1]))
             pass
         continue
 
     retval = [ filesystems[x][0] for x in filesystems.keys() ]
     return retval
 
-def Maintain(fs, run, scrub, debug):
+def Maintain(fs, run, scrub):
     '''
     execute maintenance steps for [fs]
     '''
+    log = logging.getLogger("%s.Maintain" % LOGROOT)
     for x in range(2):
-        scrubrun = _cmd_btrfsScrubStatus(fs, debug)
+        scrubrun = _cmd_btrfsScrubStatus(fs)
         if ScrubIsRunning(scrubrun):
-            click.echo("Error: Existing Scrub Running.")
-            sys.exit(1)
-        elif _cmd_btrfsBalanceRetval(fs, debug):
-            click.echo("Error: Existing Balance Running.")
-            sys.exit(1)
+            log.error("Existing Scrub Running for %s" % fs)
+            click.echo("Error: Existing Scrub Running on %s." % fs)
+            return False
+        elif _cmd_btrfsBalanceRetval(fs):
+            log.error("Existing Balance Running for %s" % fs)
+            click.echo("Error: Existing Balance Running on %s" % fs)
+            return False
         continue
     
     ### Balance Metadata
-    run("btrfs balance start -musage=0 -v %s" % fs, debug)
-    if ScrubIsRunning(_cmd_btrfsScrubStatus(fs, debug)) or _cmd_btrfsBalanceRetval(fs, debug):
-        click.echo("Error: Existing Scrub or Balance Running.")
-        sys.exit(1)
-        pass
-    run("btrfs balance start -musage=20 -v %s" % fs, debug)
-    if ScrubIsRunning(_cmd_btrfsScrubStatus(fs, debug)) or _cmd_btrfsBalanceRetval(fs, debug):
-        click.echo("Error: Existing Scrub or Balance Running.")
-        sys.exit(1)
-        pass
+    run("btrfs balance start -musage=0 -v %s" % fs)
+    if ScrubIsRunning(_cmd_btrfsScrubStatus(fs)) or _cmd_btrfsBalanceRetval(fs):
+        log.error("Existing Balance or Scrub running on %s." % fs)
+        return False
+    run("btrfs balance start -musage=20 -v %s" % fs)
+    if ScrubIsRunning(_cmd_btrfsScrubStatus(fs)) or _cmd_btrfsBalanceRetval(fs):
+        log.error("Existing Balance or Scrub running on %s." % fs)
+        return False
     ### Balance Data
-    run("btrfs balance start -dusage=0 -v %s" % fs, debug)
-    if ScrubIsRunning(_cmd_btrfsScrubStatus(fs, debug)) or _cmd_btrfsBalanceRetval(fs, debug):
-        click.echo("Error: Existing Scrub or Balance Running.")
-        sys.exit(1)
-        pass
-    run("btrfs balance start -dusage=20 -v %s" % fs, debug)
-    if ScrubIsRunning(_cmd_btrfsScrubStatus(fs, debug)) or _cmd_btrfsBalanceRetval(fs, debug):
-        click.echo("Error: Existing Scrub or Balance Running.")
-        sys.exit(1)
-        pass
+    run("btrfs balance start -dusage=0 -v %s" % fs)
+    if ScrubIsRunning(_cmd_btrfsScrubStatus(fs)) or _cmd_btrfsBalanceRetval(fs):
+        log.error("Existing Balance or Scrub running on %s." % fs)
+        return False
+    run("btrfs balance start -dusage=20 -v %s" % fs)
+    if ScrubIsRunning(_cmd_btrfsScrubStatus(fs)) or _cmd_btrfsBalanceRetval(fs):
+        log.error("Existing Balance or Scrub running on %s." % fs)
+        return False
     ### Scrub disks
-    if scrub: run("ionice -c 3 nice -10 btrfs scrub start -Bd %s" % fs, debug)
-    
+    if scrub:
+        log.debug("spawn scrub instance for %s." % fs)
+        run("ionice -c 3 nice -10 btrfs scrub start -Bd %s" % fs)
+        log.debug("Scrub complete for %s." % fs)
+        pass
     return True
 
 
@@ -239,47 +250,62 @@ def Maintain(fs, run, scrub, debug):
               help = "Enable/Disable scrubbing. (default: enabled)")
 @click.argument('filesystems', default = [])
 @click.pass_context
-def Main(ctx, silent, debug, test, allfs, scrub, filesystems):
+def Main(ctx, silent, test, allfs, scrub, filesystems, debug):
     '''
     perform btrfs filesystem maintenace for one, many or [-a]ll filesystems.
     '''
     global DEBUG, _log
     DEBUG=debug
 
-    if debug:
-        click.echo("allfs: %s" % str(allfs))
-        click.echo("debug: %s" % str(debug))
-        click.echo("silent: %s"  % str(silent))
-        click.echo("test: %s"  % str(test))
-        click.echo("filesystems: %s" % str(filesystems))
-        pass
+    _log = logging.getLogger(LOGROOT)
+    _log.setLevel(logging.INFO)
+    sysh = SysLogHandler(address="/dev/log")
+    outh = logging.StreamHandler(sys.stdout)
     
-    _log = logging.getLogger()
+    if debug:
+        logfmt = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')    
+        sysh.setLevel(logging.DEBUG)
+        outh.setLevel(logging.DEBUG)
+        click.echo("arg allfs: %s" % str(allfs))
+        click.echo("arg debug: %s" % str(debug))
+        click.echo("arg silent: %s"  % str(silent))
+        click.echo("arg test: %s"  % str(test))
+    else:
+        logfmt = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        sysh.setLevel(logging.INFO)
+        outh.setLevel(logging.INFO)
+        
+    sysh.setFormatter(logfmt)
+    outh.setFormatter(logfmt)
+    
     if len(filesystems) == 0 and not allfs:
-        print("You must either use '-a' or specify a filesystem.")
-        print("")
-        print(ctx.get_help())
+        click.Echo("You must either use '-a' or specify a filesystem.")
+        click.Echo("")
+        click.Echo(ctx.get_help())
         exit()
         
-    if test:     run = TestRun
-    elif silent: run = LoggerRun
-    else:        run = InteractiveRun
-    
-    if run == InteractiveRun:
-        click.echo("Run is set to Interactive")
-        _log.addHandler(SysLogHandler(address="/dev/log"))
-        _log.addHandler(logging.StreamHandler(sys.stdout))
-    if run == TestRun:
+    if test:
+        run = TestRun
         click.echo("Run is set to Test")
-    if run == LoggerRun:
-        _log.addHandler(SysLogHandler(address="/dev/log"))
+        _log.addHandler(sysh)
+        _log.addHandler(outh)
+    elif silent:
+        run = LoggerRun
+        _log.addHandler(sysh)
         click.echo("Run is set to Logger")
-        pass
-    
-    if allfs: filesystems = locateBtrfs()
-    
+    else:
+        run = InteractiveRun
+        click.echo("Run is set to Interactive")
+        _log.addHandler(sysh)
+        _log.addHandler(outh)
+
+    if allfs:
+        filesystems = locateBtrfs()
+
+    _log.debug("arg filesystems: %s" % str(filesystems))
+
     for fs in filesystems:
-        Maintain(fs, run, scrub, debug)
+        Maintain(fs, run, scrub)
         continue
     
     return True
